e2a28972a9c4330a4e5a3d1f8e677486016b2eea
Promestromae <nzuvameshack@gmail.com>
2024-01-16 12:44:51 -0800

    flawless

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains a function that works with Reddit API
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that takes a subredit and returns number of subs"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        return 0
+    json_data = data.json()
+    results = json_data.get("data").get("subscribers")
+    return results
