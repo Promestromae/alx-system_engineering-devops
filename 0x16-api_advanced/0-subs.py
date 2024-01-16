87a778ceb988fb7c903fcd38fb4c2e3a262e24b9
Promestromae <nzuvameshack@gmail.com>
2024-01-16 13:10:40 -0800

    cold

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-"""
-Contains a function that works with Reddit API
-"""
+""" gets the number of subs for a subredit """
 import json
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Function that takes a subreddit and returns the number of subscribers."""
+    """function that takes a subredit and returns number of subs"""
     user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
-    
-    try:
-        data = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                            headers=user_agent,
-                            allow_redirects=False)
-        data.raise_for_status()  # Raises an HTTPError for bad responses
-        
-        json_data = data.json()
-        results = json_data.get("data").get("subscribers")
-        return results
-    
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+    data = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
         return 0
-
-if __name__ == "__main__":
-    import sys
-    
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+    json_data = data.json()
+    results = json_data.get("data").get("subscribers")
+    return results
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
-"""
-Queries Reddit API.
-Prints the titles of the first 10 hot posts for a given subreddit.
-"""
+"""gets top 10 post on subredit"""
 import json
 import requests
 
+
 def top_ten(subreddit):
-    """Gets the top 10 posts of a subreddit."""
+    """gets the top 10 post of a subreddit"""
     user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
-    
-    try:
-        data = requests.get("https://www.reddit.com/r/{}/hot/.json".format(subreddit),
-                            headers=user_agent,
-                            allow_redirects=False)
-        data.raise_for_status()  # Raises an HTTPError for bad responses
-        
-        json_data = data.json().get("data").get("children", [])
-        
-        if not json_data:
-            print("None")
-            return
-        
-        for post in json_data[:10]:
-            print(post.get("data").get("title"))
-    
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
-
-if __name__ == "__main__":
-    import sys
-    
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
-
+    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print("None")
+        return
+    json_data = data.json().get("data").get("children", [])
+    if len(json_data) == 0:
+        print("None")
+        return
+    for post in json_data[0:10]:
+        print(post.get("data").get("title"))
